023ea6aa7b5c6b979693df1c4ca334ca88a9c555
bsw <bsw@localhost>
2018-08-01 18:05:18 +0200

    Configureable text when rejected because of age

--- app/main/registration/register_rejected_age.lua
+++ app/main/registration/register_rejected_age.lua
@@ -4,15 +4,18 @@ app.html_title.title = _"Self registration"
 ui.container{ attr = { class = "mdl-grid" }, content = function()
   ui.container{ attr = { class = "mdl-cell mdl-cell--12-col" }, content = function()
 
-    ui.heading{ content = _"Registration rejected" }
-    slot.put("<br />")
-    ui.container { content = function()
-      ui.tag { content = _"Sorry, but you need to be at least 16 years old to participate. You can " }
-      ui.link{ content = _"browse the platform as a guest", module = "index", view = "index" }
-      ui.tag{ content = "." }
-    end }
-    slot.put("<br />")
-
+    if config.self_registration.rejected_age_text then
+      slot.put(config.self_registration.rejected_age_text)
+    else
+      ui.heading{ content = _"Registration rejected" }
+      slot.put("<br />")
+      ui.container { content = function()
+        ui.tag { content = _"Sorry, but you need to be at least 16 years old to participate. You can " }
+        ui.link{ content = _"browse the platform as a guest", module = "index", view = "index" }
+        ui.tag{ content = "." }
+      end }
+      slot.put("<br />")
+    end
     
   end }
 end }
