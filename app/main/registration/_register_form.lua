7aa18ddf85c1667e9451170a184c6c85b43f2446
bsw <bsw@localhost>
2018-08-08 17:14:44 +0200

    Work around layout issue

--- app/main/registration/_register_form.lua
+++ app/main/registration/_register_form.lua
@@ -91,10 +91,9 @@ for i, field in ipairs(config.self_registration.fields) do
       ui.tag{ tag = "label", content = field.label }
       ui.tag{ tag = "input", attr = { type = "file", name = "verification_data_" .. field.name } }
       if field.optional_checkbox then
-        ui.tag{ tag = "input", attr = {
+        ui.tag{ tag = "input", content = "", attr = {
           name = "verification_data_" .. field.name .. "_optout",
           checked = request.get_param{ name = "verification_data_" .. field.name .. "_optout" } and true or false,
-          content = ""
         } }
         slot.put(" ")
         ui.tag{ content = field.optional_checkbox }
