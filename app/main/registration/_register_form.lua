d8686624a216d665d94b5d372d597902d46a2226
bsw <bsw@localhost>
2018-08-13 19:37:22 +0200

    Changed style of register image upload 10

--- app/main/registration/_register_form.lua
+++ app/main/registration/_register_form.lua
@@ -127,26 +127,9 @@ for i, field in ipairs(config.self_registration.fields) do
         }
       ]] }
       ui.tag{ tag = "input", attr = { id = "fileInput", style = "display: none;", type = "file", name = "verification_data_" .. field.name, onchange = "fileChoosen(this);" } }
-      ui.tag{ tag = "label", attr = {
-          class = "mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect",
-          style = "display: inline;",
-        },
-        content = function()
-          ui.tag{
-            tag = "input",
-            attr = {
-              type = "checkbox",
-              class = "mdl-checkbox__input",
-              id = "fileCheckbox",
-              style = "float: left;",
-              onclick = "getFile();"
-            }
-          }
-          ui.tag{
-            attr = { id = "fileBtn", class = "mdl-checkbox__label", onclick = "getFile();"  },
-            content = field.upload_label
-          }
-        end
+      ui.tag{
+        attr = { id = "fileBtn", class = "mdl-checkbox__label", onclick = "getFile();"  },
+        content = field.upload_label
       }
       if field.optional_checkbox then
         slot.put(" &nbsp; ")
