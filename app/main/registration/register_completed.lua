1c7310fcd5f60d6cb28160ecdb3084161469123f
bsw <bsw@localhost>
2018-08-17 18:55:07 +0200

    Allow extra content on registration completed page

--- app/main/registration/register_completed.lua
+++ app/main/registration/register_completed.lua
@@ -10,7 +10,9 @@ ui.container{ attr = { class = "mdl-grid" }, content = function()
     slot.put("<br />")
     ui.container { content = _"Please also check your SPAM folder." }
     slot.put("<br />")
-
+    if config.self_registration.confirmation_extra_html then
+      slot.put(config.self_registration.confirmation_extra_html)
+    end
     
   end }
 end }
