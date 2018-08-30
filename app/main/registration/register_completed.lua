a52cb32953ef63024e0ee382f2a3ac65a625535c
bsw <bsw@localhost>
2018-08-30 10:35:26 +0200

    Allow full replacement of registration confirmation page

--- app/main/registration/register_completed.lua
+++ app/main/registration/register_completed.lua
@@ -4,14 +4,18 @@ app.html_title.title = _"Self registration"
 ui.container{ attr = { class = "mdl-grid" }, content = function()
   ui.container{ attr = { class = "mdl-cell mdl-cell--12-col" }, content = function()
 
-    ui.heading{ content = _"Self registration completed" }
-    slot.put("<br />")
-    ui.container { content = _"We have sent you an invitation email to finish the account setup." }
-    slot.put("<br />")
-    ui.container { content = _"Please also check your SPAM folder." }
-    slot.put("<br />")
-    if config.self_registration.confirmation_extra_html then
-      slot.put(config.self_registration.confirmation_extra_html)
+    if config.self_registration.confirmation_html then
+      slot.put(config.self_registration.confirmation_html)
+    else
+      ui.heading{ content = _"Self registration completed" }
+      slot.put("<br />")
+      ui.container { content = _"We have sent you an invitation email to finish the account setup." }
+      slot.put("<br />")
+      ui.container { content = _"Please also check your SPAM folder." }
+      slot.put("<br />")
+      if config.self_registration.confirmation_extra_html then
+        slot.put(config.self_registration.confirmation_extra_html)
+      end
     end
     
   end }
