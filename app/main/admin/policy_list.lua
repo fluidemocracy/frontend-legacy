1431210293064bbfb5c942f7be7551d79e386f50
bsw <bsw@localhost>
2018-10-18 17:33:11 +0200

    New layout for admin policy list view

--- app/main/admin/policy_list.lua
+++ app/main/admin/policy_list.lua
@@ -5,56 +5,55 @@ local policies = Policy:build_selector{ active = not show_not_in_use }:exec()
 
 ui.titleAdmin(_"Policy list")
 
-ui.section( function()
-  ui.sectionHead( function()
-    ui.heading { level = 1, content = _"Policy list" }
-  end )
-  
-  ui.sectionRow( function()
+ui.grid{ content = function()
 
-    if show_not_in_use then
-      ui.link{
-        text = _"Show policies in use",
-        module = "admin",
-        view = "policy_list"
-      }
+  ui.cell_main{ content = function()
+    ui.container{ attr = { class = "mdl-card mdl-card__fullwidth mdl-shadow--2dp" }, content = function()
+      ui.container{ attr = { class = "mdl-card__title mdl-card--border" }, content = function()
+        ui.heading { attr = { class = "mdl-card__title-text" }, level = 2, content = _"Policy list" }
+      end }
+      ui.container{ attr = { class = "mdl-card__content" }, content = function()
 
-    else
-      ui.link{
-        text = _"Create new policy",
-        module = "admin",
-        view = "policy_show"
-      }
-      slot.put(" &middot; ")
-      ui.link{
-        text = _"Show policies not in use",
-        module = "admin",
-        view = "policy_list",
-        params = { show_not_in_use = true }
-      }
+        if show_not_in_use then
+          ui.link{
+            text = _"Show policies in use",
+            module = "admin",
+            view = "policy_list"
+          }
 
-    end
+        else
+          ui.link{
+            text = _"Create new policy",
+            module = "admin",
+            view = "policy_show"
+          }
+          slot.put(" &middot; ")
+          ui.link{
+            text = _"Show policies not in use",
+            module = "admin",
+            view = "policy_list",
+            params = { show_not_in_use = true }
+          }
 
-  end )
-  
-  ui.sectionRow( function()
+        end
 
-    ui.list{
-      records = policies,
-      columns = {
+        ui.list{
+          records = policies,
+          columns = {
 
-        { content = function(record)
-            ui.link{
-              text = record.name,
-              module = "admin",
-              view = "policy_show",
-              id = record.id
+            { content = function(record)
+                ui.link{
+                  text = record.name,
+                  module = "admin",
+                  view = "policy_show",
+                  id = record.id
+                }
+              end
             }
-          end
+
+          }
         }
-
-      }
-    }
-
-  end )
-end )
+      end }
+    end }
+  end }
+end }
