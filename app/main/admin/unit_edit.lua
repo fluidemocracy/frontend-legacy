aa3fd9eae168d5bb28f89280a6b1568ae6455cfa
bsw <bsw@localhost>
2018-10-18 17:40:09 +0200

    New layout for admin unit and area view

--- app/main/admin/unit_edit.lua
+++ app/main/admin/unit_edit.lua
@@ -14,40 +14,52 @@ for i, unit in ipairs(Unit:get_flattened_tree()) do
   units[#units+1] = { id = unit.id, name = unit.name }
 end
 
-ui.form{
-  attr = { class = "vertical section" },
-  module = "admin",
-  action = "unit_update",
-  id = unit and unit.id,
-  record = unit,
-  routing = {
-    default = {
-      mode = "redirect",
-      modules = "admin",
-      view = "index"
-    }
-  },
-  content = function()
-    ui.sectionHead( function()
-      ui.heading { level = 1, content = unit and unit.name or _"New organizational unit" }
-    end )
-    ui.sectionRow( function()
-      ui.field.select{
-        label = _"Parent unit",
-        name = "parent_id",
-        foreign_records = units,
-        foreign_id      = "id",
-        foreign_name    = "name"
-      }
-      ui.field.text{     label = _"Name",         name = "name" }
-      ui.field.text{     label = _"Description",  name = "description", multiline = true }
-      ui.field.text{     label = _"External reference",  name = "external_reference" }
-      ui.field.boolean{  label = _"Active?",      name = "active", value = hint and true or nil }
+ui.grid{ content = function()
 
-      slot.put("<br />")
-      ui.submit{         text  = _"update unit" }
-      slot.put(" ")
-      ui.link{ module = "admin", view = "index", content = _"cancel" }
-    end )
-  end
-}
+  ui.cell_main{ content = function()
+    ui.container{ attr = { class = "mdl-card mdl-card__fullwidth mdl-shadow--2dp" }, content = function()
+      ui.container{ attr = { class = "mdl-card__title mdl-card--border" }, content = function()
+        ui.heading { attr = { class = "mdl-card__title-text" }, level = 2, content = _"Member list" }
+      end }
+      ui.container{ attr = { class = "mdl-card__content" }, content = function()
+        ui.form{
+          attr = { class = "vertical section" },
+          module = "admin",
+          action = "unit_update",
+          id = unit and unit.id,
+          record = unit,
+          routing = {
+            default = {
+              mode = "redirect",
+              modules = "admin",
+              view = "index"
+            }
+          },
+          content = function()
+            ui.sectionHead( function()
+              ui.heading { level = 1, content = unit and unit.name or _"New organizational unit" }
+            end )
+            ui.sectionRow( function()
+              ui.field.select{
+                label = _"Parent unit",
+                name = "parent_id",
+                foreign_records = units,
+                foreign_id      = "id",
+                foreign_name    = "name"
+              }
+              ui.field.text{     label = _"Name",         name = "name" }
+              ui.field.text{     label = _"Description",  name = "description", multiline = true }
+              ui.field.text{     label = _"External reference",  name = "external_reference" }
+              ui.field.boolean{  label = _"Active?",      name = "active", value = hint and true or nil }
+
+              slot.put("<br />")
+              ui.submit{         text  = _"update unit" }
+              slot.put(" ")
+              ui.link{ module = "admin", view = "index", content = _"cancel" }
+            end )
+          end
+        }
+      end }
+    end }
+  end }
+end }
