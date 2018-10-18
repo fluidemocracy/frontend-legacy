06c1dfd426a0e5d38eab65f1546d71fa636a5aa0
bsw <bsw@localhost>
2018-10-18 17:18:19 +0200

    Fixed wrong nesting

--- app/main/admin/index.lua
+++ app/main/admin/index.lua
@@ -10,56 +10,56 @@ ui.grid{ content = function()
     ui.container{ attr = { class = "mdl-card mdl-card__fullwidth mdl-shadow--2dp" }, content = function()
       ui.container{ attr = { class = "mdl-card__title mdl-card--border" }, content = function()
         ui.heading { attr = { class = "mdl-card__title-text" }, level = 2, content = _"Organizational units and subject areas" }
-        ui.container{ attr = { class = "mdl-card__content" }, content = function()
+      end }
+      ui.container{ attr = { class = "mdl-card__content" }, content = function()
 
-          for i_unit, unit in ipairs(units) do
-            ui.container { 
-              attr = { style = "margin-left: " .. ((unit.depth - 1)* 2) .. "em;" },
-              content = function ()
-                ui.heading { level = 1, content = function ()
-                  local class
-                  if unit.active == false then
-                    class = "inactive"
-                  end
-                  ui.link{ attr = { class = class }, text = unit.name, module = "admin", view = "unit_edit", id = unit.id }
-                end }
-                ui.tag { tag = "ul", attr = { class = "ul" }, content = function ()
-                  local areas
-                  if not inactive then
-                    areas = unit:get_reference_selector("areas"):add_order_by("name"):add_where("active"):exec()
-                  else
-                    areas = unit:get_reference_selector("areas"):add_order_by("name"):exec()
-                  end
-                  for i, area in ipairs(areas) do
-                    ui.tag { tag = "li", content = function ()
-                      local class
-                      if area.active == false then
-                        class = "inactive"
-                      end
-                      ui.link{ attr = { class = class }, text = area.name, module = "admin", view = "area_show", id = area.id }
-                    end }
-                  end
+        for i_unit, unit in ipairs(units) do
+          ui.container { 
+            attr = { style = "margin-left: " .. ((unit.depth - 1)* 2) .. "em;" },
+            content = function ()
+              ui.heading { level = 1, content = function ()
+                local class
+                if unit.active == false then
+                  class = "inactive"
+                end
+                ui.link{ attr = { class = class }, text = unit.name, module = "admin", view = "unit_edit", id = unit.id }
+              end }
+              ui.tag { tag = "ul", attr = { class = "ul" }, content = function ()
+                local areas
+                if not inactive then
+                  areas = unit:get_reference_selector("areas"):add_order_by("name"):add_where("active"):exec()
+                else
+                  areas = unit:get_reference_selector("areas"):add_order_by("name"):exec()
+                end
+                for i, area in ipairs(areas) do
                   ui.tag { tag = "li", content = function ()
-                    ui.link { module = "admin", view = "area_show", params = { unit_id = unit.id }, content = _"+ add new subject area" }
+                    local class
+                    if area.active == false then
+                      class = "inactive"
+                    end
+                    ui.link{ attr = { class = class }, text = area.name, module = "admin", view = "area_show", id = area.id }
                   end }
-                  slot.put("<br />")
+                end
+                ui.tag { tag = "li", content = function ()
+                  ui.link { module = "admin", view = "area_show", params = { unit_id = unit.id }, content = _"+ add new subject area" }
                 end }
-              end
-            }
-          end
-          
-          slot.put("<br />")
-          ui.link { module = "admin", view = "unit_edit", content = _"Create new unit" }
-          slot.put("<br />")
-          slot.put("<br />")
-          
-          if (not inactive) then
-            ui.link { module = "admin", view = "index", params = { inactive = true }, content = _"Show inactive" }
-          else
-            ui.link { module = "admin", view = "index", content = _"Hide inactive" }
-          end
-      
-         end }
+                slot.put("<br />")
+              end }
+            end
+          }
+        end
+        
+        slot.put("<br />")
+        ui.link { module = "admin", view = "unit_edit", content = _"Create new unit" }
+        slot.put("<br />")
+        slot.put("<br />")
+        
+        if (not inactive) then
+          ui.link { module = "admin", view = "index", params = { inactive = true }, content = _"Show inactive" }
+        else
+          ui.link { module = "admin", view = "index", content = _"Hide inactive" }
+        end
+    
       end }
     end }
   end }
