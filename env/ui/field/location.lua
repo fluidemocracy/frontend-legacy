ffe03f2050f61fe9101560c03a40a4f89f46ca23
bsw <bsw@localhost>
2020-08-20 15:08:00 +0200

    Added object reference for geo references

--- env/ui/field/location.lua
+++ env/ui/field/location.lua
@@ -13,7 +13,11 @@ function ui.field.location(args)
         tag = "input",
         attr = { type = "hidden", name = args.name, value = args.value, id = "ui_field_location_value" }
       }
-      ui.tag{ tag = "iframe", attr = { src = config.firstlife.inputmap_url .. "/src/index.html?domain=" .. request.get_absolute_baseurl() .. "&" .. config.firstlife.coordinates .. "&lightArea=false&contrast=false&mode=lite", id = "ui_field_location", class = "ui_field_location" }, content = "" }
+      ui.tag{
+        tag = "input",
+        attr = { type = "hidden", name = args.name .. "_marker_link", value = args.value_marker_link, id = "ui_field_location_marker_link" }
+      }
+      ui.tag{ tag = "iframe", attr = { src = config.firstlife.inputmap_url .. "/src/index.html?domain=" .. request.get_absolute_baseurl() .. "&" .. config.firstlife.coordinates .. "&lightArea=false&contrast=false&mode=lite&onlypointer=false", id = "ui_field_location", class = "ui_field_location" }, content = "" }
       
       ui.script{ script = [[
 
@@ -21,12 +25,14 @@ function ui.field.location(args)
           if (e.origin !== "]] .. config.firstlife.inputmap_url .. [[") return;
           var data = e.data;
           if (data.src == "InputMap") {
+            var feature = { "type": "Point", "coordinates": [data.lng, data.lat], "zoom_level": data.zoom_level };
+            if (data.feature) {
+              feature.marker_link = data.feature.marker_link;
+            }
             var el = document.getElementById("ui_field_location_value");
-            el.value = JSON.stringify({ "type": "Point", "coordinates": [data.lng, data.lat], "zoom_level": data.zoom_level });
-            console.log(el.value);
+            el.value = JSON.stringify(feature);
           }
         });
-        
       ]] }
     end)
   end
