36509a137299b28d63876f7123a11e81a8f3d4b4
bsw <bsw@localhost>
2020-08-20 14:25:34 +0200

    Automatically fix inconsistent session cookies

--- app/main/_filter/20_session.lua
+++ app/main/_filter/20_session.lua
@@ -6,6 +6,19 @@ if
   and not (request.get_module() == "oauth" and request.get_view() == "session")
 then
   slot.put_into("error", _"Cookie error. Try restarting your web browser and login again.")  
+  ui.script{ script = [[
+  function cookie_by_name(name) {
+    var match = document.cookie.match(new RegExp('(^| )' + name + '=([^;]+)'));
+    if (match) return match[2];
+  }
+  var cookie = (cookie_by_name("]] .. config.cookie_name .. [["));
+  var cookie_samesite = (cookie_by_name("]] .. config.cookie_name_samesite ..[["));
+  if (cookie != cookie_samesite) {
+    document.cookie = "]] .. config.cookie_name .. [[= ; expires = Thu, 01 Jan 1970 00:00:00 GMT"
+    document.cookie = "]] .. config.cookie_name_samesite .. [[= ; expires = Thu, 01 Jan 1970 00:00:00 GMT"
+    window.location = "]] .. request.get_absolute_baseurl() .. [[";
+  }
+  ]]}
   return
 end
 
