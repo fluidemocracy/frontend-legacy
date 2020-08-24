b47837ff6b241af8ade4a5d027ae3c0bf72e2594
bsw <bsw@localhost>
2020-08-24 13:48:03 +0200

    Added login api interface and login scope

---
+++ app/main/api/login.lua
@@ -0,0 +1,25 @@
+if not app.scopes.login then
+  request.redirect{ external = request.get_absolute_baseurl() .. "index/login.html" }
+  return
+end
+
+if not app.access_token.used then
+  local result = util.login(app.access_token.member)
+
+  if not result then
+    request.redirect{ external = request.get_absolute_baseurl() .. "index/login.html" }
+    return
+  end
+  app.access_token.used = true
+  app.access_token:save()
+end
+
+local redir_url = param.get("redir_url")
+
+if not redir_url then
+  request.redirect{ external = request.get_absolute_baseurl() }
+  return
+end
+
+request.redirect{ external = redir_url }
+
