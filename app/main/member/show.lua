fdda9a0139e3826d6dfde35cf843307eef5a427e
bsw <bsw@localhost>
2020-09-21 18:46:48 +0200

    Move language chooser on profile page

--- app/main/member/show.lua
+++ app/main/member/show.lua
@@ -64,11 +64,11 @@ ui.titleMember(member)
 ui.grid{ content = function()
   ui.cell_main{ content = function()
 
-    if app.session.member_id == member.id then
-      execute.view{ module = "index", view = "_lang_chooser" }
-    end
-
     ui.container{ attr = { class = "mdl-card mdl-card__fullwidth mdl-shadow--2dp" }, content = function()
+ 
+      if app.session.member_id == member.id then
+        execute.view{ module = "index", view = "_lang_chooser" }
+      end
       
       ui.container{ attr = { class = "mdl-card__title mdl-card--border" }, content = function()
         ui.heading { attr = { class = "mdl-card__title-text" }, level = 2, content = function()
