656dc76c16cc80cb4ef7c2396ac452fe063fff75
bsw <bsw@localhost>
2020-10-22 11:52:15 +0200

    Allow cross site request to validate endpoint

--- app/main/http_options.lua
+++ app/main/http_options.lua
@@ -3,7 +3,9 @@ if not request._route then
   return
 end
 
-if request.get_module() == "oauth2" and request.get_view() == "session" then
+if request.get_module() == "oauth2" and 
+  (request.get_view() == "session" or request.get_view() == "validate")
+then 
   local origin = request.get_header("Origin")
   if origin then
     request.add_header("Access-Control-Allow-Origin", origin)
