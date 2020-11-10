e144e49fef30716f5d06d5b7d6b4281f68a7d5dd
bsw <bsw@localhost>
2020-11-10 21:22:01 +0100

    Show incoming and outgoing delegations in member view

--- app/main/member/show.lua
+++ app/main/member/show.lua
@@ -161,7 +161,7 @@ ui.grid{ content = function()
         end }
       end }
     end
-    --[[
+
     ui.container{ attr = { class = "mdl-card mdl-card__fullwidth mdl-shadow--2dp" }, content = function()
       ui.container{ attr = { class = "mdl-card__title mdl-card--border" }, content = function()
         ui.heading { attr = { class = "mdl-card__title-text" }, level = 2, content = _"Outgoing delegations" }
@@ -185,7 +185,7 @@ ui.grid{ content = function()
         }
       end }
     end }
-    --]]
+
   end }
     
   ui.cell_sidebar{ content = function()
