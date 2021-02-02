74d695bfab0ec2506e0e9f28c3b56a9349e3e536
bsw <bsw@localhost>
2021-02-02 12:42:54 +0100

    Animate interest button

--- static/js/xhr.js
+++ static/js/xhr.js
@@ -1,9 +1,22 @@
-function toggleInterest(issue_id, iconElId) {
-  var iconEl = document.getElementById(iconElId);
+function toggleInterest(issueId) {
+  var linkEl = document.getElementById("issue_" + issueId + "_interest_link");
+  var iconEl = document.getElementById("issue_" + issueId + "_interest_icon");
   var interested = iconEl.innerHTML == "star_outline";
+
+  if (interested) {
+    linkEl.classList.add("mdl-button--accent");
+    linkEl.classList.add("mdl-button--feature-on");
+    linkEl.classList.remove("mdl-button--feature-off");
+    iconEl.innerHTML = "star";
+  } else {
+    linkEl.classList.remove("mdl-button--accent");
+    linkEl.classList.remove("mdl-button--feature-on");
+    linkEl.classList.add("mdl-button--feature-off");
+    iconEl.innerHTML = "star_outline";
+  }
   
   var data = new FormData();
-  data.append("issue_id", issue_id);
+  data.append("issue_id", issueId);
   data.append("interested", interested);
 
   fetch("/lf/interest/xhr_update", {
@@ -11,8 +24,8 @@ function toggleInterest(issue_id, iconElId) {
       body: data
   }).then(
       response => {
-        if (response.status == 200) {
-          iconEl.innerHTML = interested ? "star" : "star_outline";
+        if (response.status != 200) {
+          window.alert("Error during update");
         }
       }
   );
