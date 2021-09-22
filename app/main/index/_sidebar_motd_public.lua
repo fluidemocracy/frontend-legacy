e789855829cba2e58f79a2fb3a1004bfe3f89317
bsw <bsw@localhost>
2021-09-22 18:28:37 +0200

    Added public only MOTD

--- app/main/index/_sidebar_motd_public.lua
+++ app/main/index/_sidebar_motd_public.lua
@@ -5,3 +5,11 @@ if config.motd_public then
     end }
   end }
 end
+
+if not app.session.member and config.motd_only_public then
+  ui.container{ attr = { class = "mdl-special-card mdl-card__fullwidth mdl-shadow--2dp" }, content = function()
+    ui.container{ attr = { class = "mdl-card__content" }, content = function()
+      slot.put(config.motd_only_public)
+    end }
+  end }
+end
