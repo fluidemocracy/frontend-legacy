25435069cdd1fe902b5c1ab738c2438f69495237
bsw <bsw@localhost>
2021-09-27 11:01:56 +0200

    Do not format MOTD as draft

--- app/main/index/_sidebar_motd_intern_top.lua
+++ app/main/index/_sidebar_motd_intern_top.lua
@@ -2,7 +2,7 @@ if app.session.member and config.motd_intern_top then
   ui.container{ attr = { class = "mdl-card mdl-card__fullwidth mdl-shadow--2dp" }, content = function()
     ui.container{ attr = { class = "mdl-card__content" }, content = function()
       ui.container{
-        attr = { class = "draft motd" },
+        attr = { class = "motd" },
         content = function()
           slot.put(config.motd_intern_top)
         end
