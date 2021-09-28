d88c70e643b606a654db26aecd82bf89f0bb7086
bsw <bsw@localhost>
2021-09-28 09:43:58 +0200

    Render links in unit and area description, updated layout of unit and area box

---
+++ env/format/text_with_links.lua
@@ -0,0 +1,7 @@
+function format.text_with_links(value)
+  value = encode.html(value)
+  value = string.gsub(value, "http[^%s:]*://[^%s]+", function(match)
+    return "<a href=\"" .. match .. "\">" .. match .. "</a>"
+  end)
+  return value
+end
