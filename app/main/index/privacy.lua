a92637e2fd82af621944513d54f9c93e7b23ae05
bsw <bsw@localhost>
2022-01-26 00:33:21 +0100

    Better handling of use terms, added view for privacy policy

--- app/main/index/privacy.lua
+++ app/main/index/privacy.lua
@@ -1,23 +1,23 @@
-if not config.use_terms then
+if not config.privacy_policy then
   return execute.view { module = "index", view = "404" }
 end
 
-ui.title(config.use_terms_headline or _"Terms of use")
+ui.title(config.privacy_policy_headline or _"Privacy policy")
 
 ui.grid{ content = function()
   ui.cell_main{ content = function()
     ui.container { attr = { class = "mdl-card mdl-shadow--2dp mdl-card__fullwidth" }, content = function()
       ui.container{ attr = { class = "mdl-card__title mdl-card--border" }, content = function()
         ui.heading { attr = { class = "mdl-card__title-text" }, level = 2, content = function()
-           ui.tag{ content = config.use_terms_headline or _"Terms of use" }
-           if config.use_terms_subheadline then
+           ui.tag{ content = config.privacy_policy_headline or _"Privacy policy" }
+           if config.privacy_policy_subheadline then
             slot.put("<br>")
-            ui.tag{ attr = { style = "font-size: 75%;" }, content = config.use_terms_subheadline }
-           end
-         end }
+            ui.tag{ attr = { style = "font-size: 75%;" }, content = config.privacy_policy_subheadline }
+          end
+        end }
       end }
       ui.container{ attr = { class = "mdl-card__content draft" }, content = function()
-        slot.put(config.use_terms)
+        slot.put(config.privacy_policy)
       end }
     end }
   end }
