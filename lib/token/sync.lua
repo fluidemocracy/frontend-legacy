a1c1c721359f1325dc53fd7622b8a6ec9b500431
bsw <bsw@localhost>
2022-03-22 10:35:44 +0100

    More efficient algorithm

--- lib/token/sync.lua
+++ lib/token/sync.lua
@@ -22,14 +22,7 @@ function _G.sync_token()
 
   local data = json.import(output)
 
-  local privileges = Privilege:new_selector()
-    :add_where{ "unit_id = ?", config.token.unit_id }
-    :exec()
-
-  for i, privilege in ipairs(privileges) do
-    privilege:destroy()
-  end
-
+  local privileged_member_ids = {}
   for i, entry in ipairs(data.data.balanceNotificationMany) do
     local member = Member:new_selector()
       :join("member_profile", nil, "member_profile.member_id = member.id")
@@ -37,9 +30,17 @@ function _G.sync_token()
       :optional_object_mode()
       :exec()
     if member then
-      local privilege = Privilege:new()
-      privilege.unit_id = config.token.unit_id
-      privilege.member_id = member.id
+      privileged_member_ids[member.id] = true
+      local privilege = Privilege:new_selector()
+        :add_where{ "unit_id = ?", config.token.unit_id }
+        :add_where{ "member_id = ?", member.id }
+        :optional_object_mode()
+        :exec()
+      if not privilege then
+        privilege = Privilege:new()
+        privilege.unit_id = config.token.unit_id
+        privilege.member_id = member.id
+      end
       privilege.initiative_right = true
       privilege.voting_right = true
       privilege.weight = entry.amount
@@ -47,4 +48,16 @@ function _G.sync_token()
     end
   end
 
+  local privileges = Privilege:new_selector()
+    :add_where{ "unit_id = ?", config.token.unit_id }
+    :exec()
+
+  for i, privilege in ipairs(privileges) do
+    if not privileged_member_ids[privilege.member_id] then
+      privilege:destroy()
+    end
+  end
+
+
 end
+
