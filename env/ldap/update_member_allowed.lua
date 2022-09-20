e678dabdf1f1a949030c7cff8bf11910f0357736
bsw <bsw@localhost>
2022-09-20 17:35:29 +0200

    Handle missing ldap.member.allowed function

--- env/ldap/update_member_allowed.lua
+++ env/ldap/update_member_allowed.lua
@@ -1,5 +1,8 @@
 function ldap.update_member_allowed(member, ldap_entry)
-  local allowed = config.ldap.member.allowed_map(ldap_entry)
+  local allowed = true
+  if config.ldap.member.allowed_map then
+    allowed = config.ldap.member.allowed_map(ldap_entry)
+  end
   if allowed then
     member.locked = false
   else
